bcdd25a2ea77b0a05ec2f1ba40f5e585e6a5c7e6
beiweiqiang <beiweiqiang@qq.com>
2019-07-18 08:57:52 +0800

    csdn ad block

---
+++ csdn-ad-block/js/ad_block.js
@@ -0,0 +1,26 @@
+function adBlock() {
+  displayNone(document.getElementsByClassName('t0'));
+  displayNone(document.querySelectorAll('[data-pid="blog"]'));
+  displayNone(document.querySelectorAll('.comment-box'));
+  displayNone(document.querySelectorAll('.recommend-box'));
+  displayNone(document.querySelectorAll('#csdn-toolbar'));
+  displayNone(document.querySelectorAll('.vertical'));
+  displayNone(document.querySelectorAll('#asideNewComments'));
+  displayNone(document.querySelectorAll('#asideFooter'));
+  displayNone(document.getElementsByClassName('recommend-ad-box'));
+  displayNone(document.getElementsByClassName('csdn-side-toolbar'));
+  displayNone(document.getElementsByTagName('iframe'));
+
+  document.querySelector('.btn-readmore').click();
+}
+
+function displayNone(elems) {
+  for (var i = 0; i < elems.length; i++) {
+    elems[i].style.display = 'none';
+  }
+}
+
+window.addEventListener('load', function (ev) {
+  adBlock();
+});
+
